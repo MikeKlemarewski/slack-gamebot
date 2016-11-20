e2272502a5d6be2cf3408593dbaeefd3f3ff1b7f
Edward Ogmundson <edwardogmundson@edwardogmundson-5.local>
2016-11-19 22:19:46 -0800

    Changed display of values to redable dollars

--- slack-gamebot/commands/spend.rb
+++ slack-gamebot/commands/spend.rb
@@ -1,20 +1,37 @@
 module SlackGamebot
   module Commands
     class Spend < SlackRubyBot::Commands::Base
-      command 'spend', 'spent', '' do |client, data, _match|
+      command 'spend', 'spent', '' do |client, data, match|
         user = ::User.find_create_or_update_by_slack_id!(client, data.user)
-          client.say(channel: data.channel, text: "#{data.user} spent money!")
+          expression = match['expression'] if match['expression']
+          arguments = expression.split.reject(&:blank?) if expression
+          spent_credit = arguments.first.delete('$ ,')
+          new_credit = user.spend_credit(spent_credit.to_f*100)
+          new_balance = '%.2f' % [user.credit.to_f/100]
+          client.say(channel: data.channel, text: "Money well spent, #{data.user}! Your new balance is $#{new_balance}")
         logger.info "SPEND: #{client.owner} - #{data.user}"
       end
     end
+
+    class Balance < SlackRubyBot::Commands::Base
+      command 'balance', '' do |client, data, _match|
+        user = ::User.find_create_or_update_by_slack_id!(client, data.user)
+        current_balance = '%.2f' % [user.credit.to_f/100]
+          client.say(channel: data.channel, text: "Your balance is $#{current_balance}")
+        logger.info "Balance: #{client.owner} - #{data.user}"
+      end
+    end
+
     class Credit < SlackRubyBot::Commands::Base
       command 'credit', 'give', '' do |client, data, match|
         user = ::User.find_create_or_update_by_slack_id!(client, data.user)
         if user.captain?
           expression = match['expression'] if match['expression']
           arguments = expression.split.reject(&:blank?) if expression
-          new_credit = user.add_credit(arguments.first.to_i)
-          client.say(channel: data.channel, text: "Enjoy the cash, #{data.user}! Your new balance is #{new_credit}")
+          updated_credit = arguments.first.delete('$ ,')
+          new_credit = user.add_credit(updated_credit.to_f*100)
+          new_balance = '%.2f' % [user.credit.to_f/100]
+          client.say(channel: data.channel, text: "Enjoy the cash, #{data.user}! Your new balance is $#{new_balance}")
         else
           client.say(channel: data.channel, text: "#{data.user} is not a captain. I don't respect your authority.")
         end
--- slack-gamebot/models/user.rb
+++ slack-gamebot/models/user.rb
@@ -181,4 +181,10 @@ class User
     update_attributes!(credit: self.credit+amount)
     self.credit
   end
+
+  def spend_credit(amount)
+    update_attributes!(credit: self.credit-amount)
+    self.credit
+  end
+
 end
